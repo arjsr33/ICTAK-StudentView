d86f7ea00ac0f7ab3977db5f31002e1edc3e3bba
Arjun K <arjunsreedharan32@gmail.com>
2025-06-05 08:25:11 +0530

    UIUpdate-Home and Navbar

--- Frontend_ICT/src/components/Signup.jsx
+++ Frontend_ICT/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
+import Navbar from './Navbar';
 
 const Signup = () => {
   const [name, setName] = useState('');
