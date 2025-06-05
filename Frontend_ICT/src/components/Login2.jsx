9a729a9310735405f810d7d53d5ebf5769d158da
Arjun K <arjunsreedharan32@gmail.com>
2025-06-05 08:17:35 +0530

    UIUpdate-Home and Navbar

--- Frontend_ICT/src/components/Login2.jsx
+++ Frontend_ICT/src/components/Login2.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Navbar from './Navbar/Navbar';
+import Navbar from './Navbar';
 
 const Login2 = () => {
   const [email, setEmail] = useState('');
